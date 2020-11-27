51251ddfd8d29f69afc436ec9356bd89c81c280f
Naimur Rahman <naimur978@gmail.com>
2020-11-27 19:57:53 +0600

    removes test file, app.css file, changes directory location of store

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import setAuthToken from "./utils/setAuthToken";
 
 import { setCurrentUser, logoutUser } from "./actions/authActions";
 import { Provider } from "react-redux";
-import store from "./store";
+import store from "./store/store";
 
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
@@ -14,7 +14,6 @@ import Login from "./components/auth/Login";
 import PrivateRoute from "./components/private-route/PrivateRoute";
 import Dashboard from "./components/dashboard/Dashboard";
 
-import "./App.css";
 
 // Check for token to keep user logged in
 if (localStorage.jwtToken) {
